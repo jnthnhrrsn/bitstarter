5d1e0369f55c2e124be7a156d91d99d38c60dac5
Jonathan Harrison <jnthnhrrsn@gmail.com>
2013-07-07 05:40:26 +0000

    web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var inFile = "index.html";
-var buffer = new Buffer(fs.readFileSync(inFile),"utf-8");
+var buf = new Buffer(fs.readFileSync(inFile),"utf-8");
 
 var app = express.createServer(express.logger());
 
